c0ff7a83379d4b00a371e7146e6a2ac88eae94ef
bsAlex11 <bsAlex11@users.noreply.github.com>
2017-03-20 23:48:23 +0200

    Add files via upload

--- fans/fans-script.js
+++ fans/fans-script.js
@@ -1,6 +1,53 @@
 $(document).ready(function(){
 
+  /*== show sections ==*/ 
 
+ var $matchList = $("ul").eq(1).children(),
+     $targetList = $("#riv-description").children("p");
+ 
+     $matchList.on("click",function(){
+         
+        var $this = $(this),
+            data = $this.data("list");
+            
 
+           $targetList.each(function(i,elm){
+
+               if(data == $(elm).attr("id"))
+                  {  
+                        $targetList.hide();
+                        $(elm).fadeIn(500);  
+                     $matchList.removeClass("clicked");
+                     $this.addClass("clicked");
+                     
+                  }
+           });
+     });
+
+       /*== back to top button ==*/
+
+      var $toTop = $("#toTop");
+      var $window = $(window);
+      $window.on("scroll",function(){
+
+          if($window.scrollTop() > 150 )
+            $toTop.fadeIn();
+           else 
+            $toTop.fadeOut(); 
+      });
+     $toTop.on("click",function(){
+       
+        $("body,html").animate({scrollTop:0},{duration:300});
+     });  
+
+      
+      //*== drop down menu ==*//
+
+      var $button = $("#hamburger-menu");
+      var $menu = $("ul").first();
+        
+        $button.on("click",function(){
+            $menu.slideToggle(); 
+        });
 
 });
